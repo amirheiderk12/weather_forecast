bdadfdcd2f1f3f311525a9c9f06e82ad55b6364a
Amir Heider <amirheiderk@gmail.com>
2024-10-21 15:58:47 -0400

    settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,17 +19,13 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join("tmp/caching-dev.txt").exist?
-    config.action_controller.perform_caching = true
-    config.action_controller.enable_fragment_cache_logging = true
+  config.action_controller.perform_caching = true
+  config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :memory_store
-    config.public_file_server.headers = { "Cache-Control" => "public, max-age=#{2.days.to_i}" }
-  else
-    config.action_controller.perform_caching = false
+  config.cache_store = :redis_cache_store, { url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" } }
+  config.public_file_server.headers = { "Cache-Control" => "public, max-age=#{30.minutes.to_i}" }
 
-    config.cache_store = :null_store
-  end
+  config.active_job.queue_adapter = :sidekiq
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
