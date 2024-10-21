6f32301087b5d1d9ae0e08a6669545cfe22f9b17
Amir Heider <amirheiderk@gmail.com>
2024-10-21 16:06:59 -0400

    clear flashes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,14 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  before_action :clear_flash
   rescue_from StandardError, with: :handle_exception
 
   private
+
+  def clear_flash
+    flash.clear
+  end
 
   def handle_exception(exception)
     Rails.logger.error "An error occurred: #{exception.message}"
