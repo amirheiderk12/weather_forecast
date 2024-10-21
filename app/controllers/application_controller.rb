4010ef369562dc49d00c4d15b9d0b611127ef814
Amir Heider <amirheiderk@gmail.com>
2024-10-21 15:59:15 -0400

    general exception handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  rescue_from StandardError, with: :handle_exception
+
+  private
+
+  def handle_exception(exception)
+    Rails.logger.error "An error occurred: #{exception.message}"
+    flash[:error] = "Something went wrong. Please try again later."
+    redirect_to root_path
+  end
 end
