1e2bf8742bec033a89651f36355da6ef36ba028e
Amir Heider <amirheiderk@gmail.com>
2024-10-21 17:44:01 -0400

    move controller to service

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,27 +1,15 @@
 class WeatherController < ApplicationController
   def index
     if params[:zipcode].present?
-      zipcode = params[:zipcode].strip
-      if valid_zipcode?(zipcode)
-        cache_key = "weather_#{zipcode}"
-        weather_data = Rails.cache.read(cache_key)
+      weather_service = HandlerService.new(params[:zipcode].strip)
+      result = weather_service.process
 
-        if weather_data
-          @weather_data = weather_data
-          @from_cache = true
-        else
-          FetchWeatherJob.perform_later(zipcode)
-          flash[:notice] = "Weather data is being fetched for ZIP code #{zipcode}. Please refresh the page in a few moments."
-        end
+      if result[:success]
+        @weather_data = result[:data]
+        @from_cache = result[:from_cache]
       else
-        flash[:error] = "Invalid ZIP code. Please enter a valid US ZIP code."
+        flash[result[:flash_type]] = result[:message]
       end
     end
-  end
-
-  private
-
-  def valid_zipcode?(zipcode)
-    zipcode.match?(/\A\d{5}(-\d{4})?\z/)
   end
 end
